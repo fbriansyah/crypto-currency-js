4fa612da1f1e646d5daaabb506c0b3ce2e4992b2
Febriansyah <fbriansyah@gmail.com>
2023-08-25 18:27:20 +0700

    implement isValidChain method

--- blockchain.js
+++ blockchain.js
@@ -15,6 +15,26 @@ class Blockchain {
     })
     this.chain.push(newBlock);
   }
+
+  static isValidChain(chain) {
+    // check genesis block;
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false
+    };
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, data } = chain[i];
+      const actualLastHash = chain[i-1].hash;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
+  }
 }
 
 module.exports = Blockchain;
