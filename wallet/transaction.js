e29fbded9576f7d9369a1fec167a9b27ceac3ce3
Febriansyah <fbriansyah@gmail.com>
2023-08-30 09:06:06 +0700

    add validTransaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v1');
+const { verifySignature } = require('../util/ec');
 
 class Transaction {
   constructor({senderWallet, recipient, amount}) {
@@ -24,6 +25,26 @@ class Transaction {
       address: senderWallet.publicKey,
       signature: senderWallet.sign(outputMap),
     };
+  }
+
+  static validTransaction(transaction) {
+    const {input, outputMap} = transaction;
+    const {address, amount, signature} = input;
+
+    const outputTotal = Object.values(outputMap)
+      .reduce((total, outputAmount) => total + outputAmount);
+
+    if(amount !== outputTotal) {
+      console.error(`invalid transcation at address ${address}`);
+      return false
+    }
+
+    if(!verifySignature({publicKey: address, data: outputMap, signature})) {
+      console.error(`error transaction signature at address ${address}`);
+      return false;
+    }
+
+    return true;
   }
 }
 
